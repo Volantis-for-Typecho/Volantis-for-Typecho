557deab80700d75312d3202f59d6028d4e844273
cndelivr <129635690+cndelivr@users.noreply.github.com>
2023-04-27 06:42:43 +0800

    Create options.php

---
+++ options.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+  'themeName' => 'Volantis', // 主题名称
+  'themeUrl' => '', // 主题链接
+  'author' => 'Bill', // 作者名称
+  'authorUrl' => 'https://blog.1703.site', // 作者链接
+  'version' => '1.0', // 主题版本号
+  'description' => 'Typechozhu ti', // 主题描述
+);
