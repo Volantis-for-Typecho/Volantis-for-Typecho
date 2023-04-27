030bd7ba223ee236bb654e7ec0bc40a8d8ee11c3
cndelivr <129635690+cndelivr@users.noreply.github.com>
2023-04-27 19:01:58 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,59 @@
+const config = {
+  siteName: 'My Blog',
+  siteDescription: 'This is my blog powered by Typecho.',
+  siteKeywords: 'blog,Typecho',
+  siteUrl: 'https://example.com',
+  authorName: 'Your Name',
+  authorEmail: 'your.email@example.com',
+  authorAvatar: 'https://cdn.jsdelivr.net/gh/vuejs/vue-next/logo.png',
+  headerNav: [
+    {
+      name: 'Home',
+      slug: '/',
+    },
+    {
+      name: 'Category',
+      slug: '/category/',
+    },
+    {
+      name: 'Archives',
+      slug: '/archives/',
+    },
+    {
+      name: 'About',
+      slug: '/about/',
+    },
+  ],
+  footerNav: [
+    {
+      name: 'Home',
+      slug: '/',
+    },
+    {
+      name: 'Category',
+      slug: '/category/',
+    },
+    {
+      name: 'Archives',
+      slug: '/archives/',
+    },
+    {
+      name: 'About',
+      slug: '/about/',
+    },
+  ],
+  socialLinks: [
+    {
+      name: 'GitHub',
+      iconClass: 'fa fa-github',
+      linkUrl: 'https://github.com/yourusername',
+    },
+    {
+      name: 'Twitter',
+      iconClass: 'fa fa-twitter',
+      linkUrl: 'https://twitter.com/yourusername',
+    },
+    {
+      name: 'Email',
+      iconClass: 'fa fa-envelope',
+      linkUrl: 'mailto
